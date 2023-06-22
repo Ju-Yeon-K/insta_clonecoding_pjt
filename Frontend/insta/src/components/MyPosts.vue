bdd1a077c255c628c0352582f6353a543aee2a28
juyeon <7777kmg@hanmail.net>
2023-06-23 03:46:01 +0900

    profile 페이지 작성

---
+++ Frontend/insta/src/components/MyPosts.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div class="container">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div v-for="post in posts" :key="post.pk" class="col-4">
+          <div class="card m-2" style="width: 19rem;">
+            <router-link :to="{ name:'postDetail', params: {postpk : post.pk} }">
+            <img :src="'http://127.0.0.1:8000/media/'+ post.image" class="card-img-top imgsizing">
+            </router-link>
+                <div class="card-body">
+                <p class="card-text">{{post.content}}</p>
+                <span>
+                  posted by : 
+                </span>
+                <router-link :to="{ name:'profile', params: {userpk : post.user}  }" class="card-text" >{{post.user}}</router-link> 
+
+                <p class="card-text">
+                  <i class="far fa-heart" style="color: crimson;"></i>
+                  {{post.like_users}}
+                </p>
+
+                <div class="logo d-flex justify-content-end"> <!--여기 설정 -->
+                  <router-link :to="{ name:'updatepost', params: { postpk : post.pk} }">✏️Edit</router-link>
+                  <a href="#" @click.prevent="deletePost(post.pk, $event)">🗑️Delete</a>
+                </div>
+                </div> 
+          </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+const API_URL = 'http://127.0.0.1:8000'
+
+export default {
+  name:'Posts',
+  data(){
+    return {
+      posts: [],
+    }
+  },
+  computed: {
+    token(){
+      return this.$store.state.token
+    }
+  },
+  methods:{
+    getPostList(){
+      axios({
+        method: 'get',
+        url: `${API_URL}/posts/`,
+        headers: {
+          'Authorization': 'Token ' + this.token
+          }
+      })
+      .then((res) => {
+        this.posts = res.data.results
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+    deletePost(post_pk, event){
+      axios({
+        method: 'DELETE',
+        url: `${API_URL}/posts/${post_pk}/delete/`,
+        headers: {
+          'Authorization': 'Token ' + this.token
+          }
+      })
+      .then((res) => {
+        this.getPostList()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
+  },
+  created(){
+    this.getPostList()
+  },
+
+}
+</script>
+
+<style scoped>
+*{
+  text-decoration: none;
+}
+.imgsizing {
+  height: 20rem;
+}
+.card {
+  height: 31rem;
+}
+a {
+  color: black;
+}
+a:hover{
+  cursor: pointer;
+  color: rgb(117, 167, 150);
+}
+</style>
--- Frontend/insta/src/views/ProfileView.vue
+++ Frontend/insta/src/views/ProfileView.vue
@@ -2,99 +2,104 @@
   <div>
     <div class="container text-start">
       <div class="row">
-        <div class="col-12" >          
+        <div class="d-flex justify-content-between" >          
           
-          <h3 v-if="is_me">{{profile.user}} 님의 프로필</h3> <!-- {% if request.user.id == profile.user_id %}   -->
-          <h3 v-else>내 프로필</h3>
+          <span v-if="is_me" class="title">{{profile.user}} 님의 프로필</span> <!-- {% if request.user.id == profile.user_id %}   -->
+          <span v-else class="title">내 프로필</span>
+
+          <div class="mt-auto mb-0">
+          <router-link :to="{ name:'passwordChange' }">
+            <button type="submit" class="btn btn-primary mybtn">Change PW</button>
+          </router-link>
+          <router-link :to="{ name:'delete' }">
+            <button type="submit" class="btn btn-danger mybtn">Delete</button>
+          </router-link>
+          <button @click="logout" type="submit" class="btn btn-secondary mybtn">Log out</button>
+        </div>
+
+
         </div>
       </div>
     </div>
 
-<!-- <div class="container text-center">
-      <div class="row">
-        <div class="col-2" >
-          {% if profile.image_file %}
-            <img src="{{ profile.image_file.url }}" class="card-img-top imgbox mt-2">
-          {% else %}
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/340px-Default_pfp.svg.png?20220226140232" class="card-img-top imgbox mt-2">
-          
-            {% endif %}
+    <div class="container text-center">
+          <div class="row">
+            <div class="col-2" >
+                <img v-if="image_file" :src="'http://127.0.0.1:8000/media/'+ image_file" class="card-img-top imgbox mt-2"> 
+                <img v-else src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/340px-Default_pfp.svg.png?20220226140232" class="card-img-top imgbox mt-2">
+            </div>
+            <div class="col-4"></div>
+            <div class="col-2 mt-4">
+              {{}} <br>
+              게시물
+            </div>
+            <div class="col-2 mt-4">
+              {{}} <br>
+              팔로워
+            </div>
+            <div class="col-2 mt-4">
+              {{}} <br>
+              팔로잉
+            </div>
+          </div>
         </div>
-        <div class="col-4">
-        </div>
-        <div class="col-2 mt-4">
-          {{profile.user.post_set.count}} <br>
-          게시물
-        </div>
-        <div class="col-2 mt-4">
-          {{profile.user.followers.count}} <br>
-          팔로워
-        </div>
-        <div class="col-2 mt-4">
-          {{profile.user.followings.count}} <br>
-          팔로잉
-        </div>
-      </div>
-    </div>
     
-    <div class="row m-2">
+        <div v-if="nickname" class="row ms-2">{{}} </div> 
+        <div v-else class="row m-2">No NickName</div>
 
-    {% if profile.nickname %} {{profile.nickname}} {% else %}등록된 닉네임이 없어욤 ㅜㅜ
-    {% endif %}
-  </div>
-  <div class="row mx-2">
-      {% if profile.introduction %} {{profile.introduction}}{% else %}등록된 자기 소개글이 없어욤 ㅜㅜ 
-    {% endif %}
-  </div>
-  <div class="row">
-    <div class="col-2 mt-2">
-      {% if request.user != profile.user %}
-      <form action="{% url 'accounts:follow' profile.user.pk %}" method="POST">
-        {% csrf_token %}
-        {% if request.user in profile.user.followers.all %}
-          <button type="submit" class="btn btn-secondary">언팔로우</button>
-        {% else %}
-          <button type="submit" class="btn btn-secondary">팔로우</button>
-        {% endif %}
-  
-        </form>
-      {% endif %}
-    
-    </div>
-  </div>
-    {% if request.user.id == profile.user_id %}
-    <br><br>
-    <a href="{% url 'accounts:logout' %}">
-      <button type="submit" class="btn btn-secondary">Log out</button>
-    </a>
-    <a href="{% url 'accounts:profile_update' %}">
-      <button type="submit" class="btn btn-warning">Profile Edit</button>
-    </a>
-    <a href="{% url 'accounts:password' %}">
-      <button type="submit" class="btn btn-primary">Change Password</button>
-    </a>
-    <a href="{% url 'accounts:delete' %}">
-      <button type="submit" class="btn btn-danger">Delete Account</button>
-    </a>
-    {% endif %}
-    <hr>
-  </div> -->
-  </div>
+        <div v-if="introduction" class="row ms-2">{{}}</div>
+        <div v-else class="row mx-2">No Introduction</div>
+
+        <div class="row">
+          <div class="col-2 mt-2">
+            
+           <!-- <form action="{% url 'accounts:follow' profile.user.pk %}" method="POST">  {% if request.user != profile.user %}
+                <button type="submit" class="btn btn-secondary" style="width: 11rem;">팔로잉</button> {% if request.user in profile.user.followers.all %}
+                <button type="submit" class="btn btn-primary" style="width: 11rem;">팔로우</button>
+            </form> -->
+           
+          </div>
+        </div>
+
+        <Posts />
+        
+      </div>
 </template>
 
 <script>
+import Posts from '@/components/Posts.vue'
 export default {
     name:'ProfileView',
     data() {
-    return {
-      user_pk:this.$route.params.userpk,
-      is_me:false
-    }
+      return {
+        user_pk:this.$route.params.userpk,
+        is_me:false,
+        image_file:null,
+        nickname:null,
+        introduction:null,
+      }
+  },
+  components:{
+    Posts
   },
 
 }
 </script>
 
-<style>
+<style scoped>
+.mybtn {
+  width: 7rem;
+  height: 2.5rem;
+  background-color: #fff;
+  color: rgb(133, 187, 186);
+  border: none;
+}
+.mybtn:hover {
+  color: rgb(16, 52, 82);
+  background-color: #fff;
 
+}
+.title{
+  font-size: 2rem;
+}
 </style>
