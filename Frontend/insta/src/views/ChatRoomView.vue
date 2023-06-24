30a75aaa709e4f759dd9e0e63e4dbc513dc7c421
juyeon <7777kmg@hanmail.net>
2023-06-25 03:26:46 +0900

    chat vue 작성

--- Frontend/insta/src/views/ChatRoomView.vue
+++ Frontend/insta/src/views/ChatRoomView.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    {{room_id}} 번째 CHAT rOOM
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+          <div class="col-6">
+              <div class="form-group">
+                  <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/340px-Default_pfp.svg.png?20220226140232" alt=""/> -->
+                  <label for="exampleFormControlTextarea1" class="h4 pt-5">
+                      {{room_id}} 번째 CHAT rOOM</label>
+                  <textarea v-model="chatLog" class="form-control" id="chat-log" cols="100" rows="20" readonly></textarea><br>
+              </div>
+
+              <div class="input-group my-2">
+                <input type="text" class="form-control" :placeholder='`${username}으로 메시지 전송...`' aria-describedby="button-addon2" v-model="messageInput" @keyup.enter="sendMessage">
+                <button class="btn comment-post align-text-bottom" @click.prevent="sendMessage">  Send  </button>
+              </div>
+              
+          </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -13,6 +31,14 @@ export default {
     data() {
       return {
         room_id:this.$route.params.roompk,
+
+        socket:null,
+
+        messageInput:null,
+        chatLog:'',
+        messages:[],
+
+        logs:[],
       }
   },
   computed: {
@@ -24,32 +50,61 @@ export default {
     },
   },
   methods: {
-    getChatRoomList(){
+    connect() {
+        this.socket = new WebSocket(
+            `ws://127.0.0.1:8000/ws/chat/${this.room_id}/${this.username}/`
+        )
+        this.socket.onopen = () => {
+          this.logs.push({ type: 'INFO', msg: 'CONNECTED' })
+          this.disabled = false
+        }
+        this.socket.onerror = () => {
+          this.logs.push({ type: 'ERROR', msg: 'ERROR:' })
+        }
+        this.socket.onmessage = ({ data }) => {
+
+          this.chatLog += JSON.parse(data)['message'] + '\n'
+          this.logs.push({ type: 'RECV', msg: 'RECV:' + data })
+        }
+        this.socket.onclose = (msg) => {
+          this.logs.push({ type: 'ERROR', msg: 'Closed (Code: ' + msg.code + ', Message: ' + msg.reason + ')' })
+        
+      }
+    },
+    sendMessage(){
+      this.socket.send(JSON.stringify({
+          'message': this.messageInput
+      }));
+      this.messageInput = null;
+    },
+    getMessages(){
       axios({
         method: 'get',
-        url: `${API_URL}/accounts/${this.user}/`,
+        url: `${API_URL}/chat/${this.room_id}/messages/`,
         headers: {
           'Authorization': 'Token ' + this.token
           }
       })
       .then((res) => {
-        this.followers = res.data.followers
-        this.following_cnt = res.data.following_cnt
-        this.post_cnt = res.data.post_cnt
-        this.image_file = res.data.profile_info.length ? res.data.profile_info[0].image_raw : ''
-        this.nickname = res.data.profile_info.length ? res.data.profile_info[0].nickname : ''
-        this.introduction = res.data.profile_info.length ? res.data.profile_info[0].introduction : ''
+        this.messages = res.data.results
+        for(let i=this.messages.length -1 ;i > -1 ;i--){
+          this.chatLog = this.messages[i]['content'] + '\n' + this.chatLog
+        }
+        
       })
       .catch((err) => {
         console.log(err)
       })
     },
   },
-    // created(){
-    //   this.getChatRoomList()
-    // }
-
-}
+  created(){
+    this.getMessages()
+    this.connect()
+  },
+  destroyed(){
+    this.socket.close()
+  },
+  }
 </script>
 
 <style scoped>
